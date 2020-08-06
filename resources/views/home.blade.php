3fa6769d8699040c48c133d6eaac90662f9a7c96
naqiyamutmainnah <naqiyamutmainnah99@gmail.com>
2020-08-06 14:35:56 +0700

    lengkap2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>sanberbook</title>
+    <title>home</title>
 </head>
 <body>
     <div>
@@ -24,7 +24,7 @@
         <h3>Cara Bergabung ke SanberBook</h3>
         <ol>
             <li>Mengunjungi Website ini</li>
-            <li>Mendaftar di <a href="form.html">Form Sign Up</a> </li>
+            <li>Mendaftar di <a href="/register">Form Sign Up</a> </li>
             <li>Selesai!</li>
         </ol>
     </div>
